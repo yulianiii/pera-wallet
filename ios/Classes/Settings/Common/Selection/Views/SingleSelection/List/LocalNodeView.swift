c37a6a3b3672f6847675c410aa2669c55fcc7c17
yulz <yulz@me.com>
2024-01-04 10:38:51 -0500

    URL, Port and API Key validation on local node entry to avoid application crash

--- ios/Classes/Settings/Common/Selection/Views/SingleSelection/List/LocalNodeView.swift
+++ ios/Classes/Settings/Common/Selection/Views/SingleSelection/List/LocalNodeView.swift
@@ -117,13 +117,28 @@ final class LocalNodeView: UIView, UITextFieldDelegate {
     }
     
     func updatedLocalNetValues() {
-        urlString = urlTextField.text?.trimmingCharacters(in: .whitespaces) ?? ""
+        if let text = urlTextField.text, text.isValidURL {
+            urlString = text.trimmingCharacters(in: .whitespaces)
+            urlTextField.textColor = .black
+        } else {
+            urlTextField.textColor = .red
+        }
         
-        let apiKeyString = apiKeyTextField.text?.trimmingCharacters(in: .whitespaces) ?? ""
-        let keychain = KeychainAccess.Keychain(service: "com.algorand.algorand.token.private").accessibility(.whenUnlocked)
-         keychain.set(apiKeyString, for: "algodLocalToken")
+        if let text = apiKeyTextField.text, !text.contains(" ") {
+            let apiKeyString = text.trimmingCharacters(in: .whitespaces)
+            let keychain = KeychainAccess.Keychain(service: "com.algorand.algorand.token.private").accessibility(.whenUnlocked)
+             keychain.set(apiKeyString, for: "algodLocalToken")
+            apiKeyTextField.textColor = .black
+        } else {
+            apiKeyTextField.textColor = .red
+        }
         
-        portString = portTextField.text?.trimmingCharacters(in: .whitespaces) ?? ""
+        if let text = portTextField.text, !text.contains(" ") && text.isDigits {
+            portString = portTextField.text?.trimmingCharacters(in: .whitespaces) ?? ""
+            portTextField.textColor = .black
+        } else {
+            portTextField.textColor = .red
+        }
         
         let nc = NotificationCenter.default
         nc.post(name: Notification.Name.updateLocalNetNotification, object: nil)
